9e396c76cb0bc225c24ed6178bdff75270851a79
Andreas Guenzel <andreas.guenzel@exxeta.com>
2016-04-14 16:29:52 +0200

    modify URL in JAX-RS filter

--- src/main/java/com/github/leichtundkross/jee7/jaxrs/LoggingFilter.java
+++ src/main/java/com/github/leichtundkross/jee7/jaxrs/LoggingFilter.java
@@ -2,6 +2,7 @@ package com.github.leichtundkross.jee7.jaxrs;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Logger;
 
 import javax.ws.rs.container.ContainerRequestContext;
@@ -16,14 +17,21 @@ import javax.ws.rs.ext.Provider;
 @PreMatching
 public class LoggingFilter implements ContainerRequestFilter {
 
-	private static final Logger LOGGER = Logger.getLogger(CustomerClient.class.getCanonicalName());
+	private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getCanonicalName());
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		URI path = requestContext.getUriInfo().getAbsolutePath();
 		LOGGER.info("Called " + requestContext.getMethod() + " " + path);
 
-		if (!path.toString().contains("rest/customer")) {
+		if (path.toString().contains("rest/customers")) {
+			try {
+				LOGGER.info("Called deprecated URL 'customers'. Redirecting to new URL 'customer'");
+				requestContext.setRequestUri(new URI(path.toString().replaceAll("customers", "customer")));
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		} else if (!path.toString().contains("rest/customer")) {
 			requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST).build());
 		}
 	}
